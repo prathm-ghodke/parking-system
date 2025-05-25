c6b08e73957052fabf9553c17471369f685343c2
PrAtHm3545 <prathmeshghodke3545@gmail.com>
2025-05-26 00:56:03 +0530

    Added dropdowns of site, building, and floors

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
@@ -8,18 +9,25 @@ import { ISite, ResponseModel } from '../../model/user.model';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatIconModule,MatCardModule, MatDividerModule],
+  imports: [MatIconModule, MatCardModule, MatDividerModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-    TotalSpots: Number = 100;
+    TotalSpots: Number = 0;
     availableSpots: Number = 90;
     occupiedSpots: Number = 10;
     occupiedSpotsRate: Number = 10;
 
     masterService = inject(MasterService);
     siteList: ISite[] =[];
+    buildingList: any[] =[];
+    floorList: any[] =[];
+    parkingSpots: any[] = [];
+    siteID: number = 0;
+    buildingId: number = 0;
+    floorId: number = 0;
+    
     ngOnInit(): void {
       this.getSites();
     }
@@ -28,5 +36,26 @@ export class DashboardComponent implements OnInit {
       this.masterService.getSitesByClientId().subscribe((res: ResponseModel) => {
         this.siteList = res.data;
       });
+    }
+    getBuilding(){
+      if(this.siteID){
+        this.masterService.getBuildingBysiteId(this.siteID).subscribe((res: any) => {
+          this.buildingList = res.data;
+        });
+      }
+    }
+    getFloorByBuilding(){
+      if(this.buildingId){
+        this.masterService.getFloorByBuildingId(this.buildingId).subscribe((res: any) => {
+          this.floorList = res.data;
+        });
+      }
+    }
+    onFloorSelect(){
+      const floor = this.floorList.find((f:any) => f.floorId == this.floorId);
+      for(let index = 0; index < floor.totalParkingSpots; index++){
+        this.TotalSpots = floor.totalParkingSpots;
+        this.parkingSpots.push(index)
+      }
     }
 }
--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -10,8 +10,21 @@ export class MasterService {
 
   constructor(private http: HttpClient) { }
 
+  API_URL = "https://api.freeprojectapi.com/api/SmartParking/";
+
   getSitesByClientId(): Observable<ResponseModel> {
     const clientID = 1;
-    return this.http.get<ResponseModel>("https://api.freeprojectapi.com/api/SmartParking/GetSitesByClientId?id="+clientID);
+    return this.http.get<ResponseModel>(this.API_URL+"GetSitesByClientId?id="+clientID);
   }
+  
+  getBuildingBysiteId(siteID: number): Observable<ResponseModel> {
+    return this.http.get<ResponseModel>(this.API_URL+"GetBuildingBySiteId?id="+siteID);
+  }
+  getFloorByBuildingId(buildigID: number): Observable<ResponseModel> {
+    return this.http.get<ResponseModel>(this.API_URL+"GetFloorsByBuildingId?id="+buildigID);
+  }
+  getParkingSpotsByFloorId(floorId: number): Observable<ResponseModel> {
+    return this.http.get<ResponseModel>(this.API_URL+"GetAllParkingByFloor?id="+floorId);
+  }
+
 }
