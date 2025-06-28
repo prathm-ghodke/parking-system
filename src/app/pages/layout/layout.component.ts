da4e1175321a5499d0cd8b3c90b6bf9d85358506
PrAtHm3545 <prathmeshghodke3545@gmail.com>
2025-06-28 22:36:59 +0530

    Added booking modal

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,15 +1,31 @@
-import { Component, Input } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-layout',
-  imports: [MatCardModule, MatIconModule, ],
+  imports: [MatCardModule, MatIconModule, MatDialogModule],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
-
-  
   @Input() parkingSpots: [] = [];
+  readonly dialog = inject(MatDialog);
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog,{
+      width: '35rem'
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: 'book-parking.html',
+  imports: [MatDialogModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogContentExampleDialog {}
